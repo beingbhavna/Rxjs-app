9cbaea0783e0c0c6f03ce537c4f90635151ade46
Bhavna Sharma <bhavna@i2k2.com>
2024-09-03 15:15:56 +0530

    updated v18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,26 +5,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 // import { FirstComponentComponent } from './component/first-component/first-component.component';
 // import { SecondComponentComponent } from './component/second-component/second-component.component';
 // import { ThirdComponentComponent } from './component/third-component/third-component.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    // FirstComponentComponent,
-    // SecondComponentComponent,
-    // ThirdComponentComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        HeaderComponent,
+        // FirstComponentComponent,
+        // SecondComponentComponent,
+        // ThirdComponentComponent,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        FormsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
--- src/app/modules/observables/retry-operator/retry-operator.module.ts
+++ src/app/modules/observables/retry-operator/retry-operator.module.ts
@@ -3,16 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { RetryOperatorRoutingModule } from './retry-operator-routing.module';
 import { RetryOperatorComponent } from './retry-operator.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
-@NgModule({
-  declarations: [
-    RetryOperatorComponent
-  ],
-  imports: [
-    CommonModule,
-    RetryOperatorRoutingModule,
-    HttpClientModule
-  ]
-})
+@NgModule({ declarations: [
+        RetryOperatorComponent
+    ], imports: [CommonModule,
+        RetryOperatorRoutingModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class RetryOperatorModule { }
